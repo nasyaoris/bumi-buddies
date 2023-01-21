a19a64d359bbea1bbc9cf4bcbff7c7416cdea522
nasyaoris <nasyaoris@gmail.com>
2023-01-22 00:33:10 +0700

    add footer

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -14,85 +14,86 @@ import Link from 'next/link'
 // import { PARAMS } from '../../screens/Bisnis/schema';
 import useIsMobile from '../../hooks/useIsMobile';
 
-// const shortcuts = [
-//   {
-//     page: "Beranda",
-//     links: [
-//       {
-//         section: "Anagata",
-//         url: "/#anagata"
-//       },
-//       {
-//         section: "Bisnis",
-//         url: "/#bisnis"
-//       },
-//       {
-//         section: "Klien Kami",
-//         url: "/#klien"
-//       }
-//     ]
-//   },
-//   {
-//     page: "Tentang",
-//     links: [
-//       {
-//         section: "Tentang Kami",
-//         url: "/tentang/#tentang-kami"
-//       },
-//       {
-//         section: "Fokus Kami",
-//         url: "/tentang/#fokus-kami"
-//       },
-//     ]
-//   },
-//   {
-//     page: "Bisnis",
-//     links: [
-//       {
-//         section: "Security System",
-//         url: `/bisnis?section=${PARAMS.SECURITY_SYSTEM.key}`
-//       },
-//       {
-//         section: "Audio Visual",
-//         url: `/bisnis?section=${PARAMS.AUDIO_VISUAL.key}`
-//       },
-//       {
-//         section: "Telecommunication",
-//         url: `/bisnis?section=${PARAMS.TELECOMMUNICATION.key}`
-//       },
-//       {
-//         section: "Air Conditioning System",
-//         url: `/bisnis?section=${PARAMS.AIR_CONDITIONING.key}`
-//       },
-//       {
-//         section: "Integrated System",
-//         url: `/bisnis?section=${PARAMS.INTEGRATED_SYSTEM.key}`
-//       },
-//       {
-//         section: "Maintenance Service",
-//         url: `/bisnis?section=${PARAMS.MAINTENANCE.key}`
-//       },
-//     ]
-//   },
-// ]
+const shortcuts = [
+  {
+    page: "Information",
+    links: [
+      {
+        section: "Event & Class",
+        url: "/#anagata"
+      },
+      {
+        section: "About Us",
+        url: "/#bisnis"
+      },
+      {
+        section: "Collab with Us",
+        url: "/#klien"
+      }
+    ]
+  },
+  {
+    page: "Connect",
+    links: [],
+    contents: [
+      "connect@thesocialclub.id"
+    ]
+  },
+  {
+    page: "Contact",
+    links: [],
+    content: ["PT Karya Aplikasi Sosial Teknologi SCBD, Treasury Tower, 2nd Floor, Unit 2E District 8, Jl. Jenderal Sudirman No.52-53, RT.5/RW.3, Senayan, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12190"]
+  },
+  // {
+  //   page: "Social Media",
+  //   links: [],
+  //   contents: ["PT Karya Aplikasi Sosial Teknologi SCBD, Treasury Tower, 2nd Floor, Unit 2E District 8, Jl. Jenderal Sudirman No.52-53, RT.5/RW.3, Senayan, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12190"]
+  // },
+  
+]
 const Footer = () => {
   const isMobile = useIsMobile();
   
   return (
     <FooterContainer>
       <FooterContentBox borderBottom={1}>
-      <FadeUp>
+      {/* <FadeUp>
         <img src="/images/logoAnagataWhite.svg" alt="logo" style={{height: isMobile? "40px" : "71px"}} />
-      </FadeUp>
+      </FadeUp> */}
       <FadeUp>
         <InformationBox>
-          <Box maxWidth="370px">
+          {/* <Box maxWidth="370px">
             <Text variant="h3" sx={{ color: theme.palette.white[50], marginBottom: theme.spacing(3) }}>Anagata Visi Teknologi</Text>
             <Text variant="anagataRegulerMedium" sx={{ color: theme.palette.white[50]}}> 
             Jl. I Gusti Ngurah Rai, No. 14, RT.001/RW.010, Kranji, Kecamatan Bekasi Barat, Kota Bekasi, Jawa Barat 17135
             </Text>
-          </Box>
+          </Box> */}
           <ShortcutContainer>
+            {/* TODO: change to Link */}
+          <Box maxWidth="370px">
+            <Text variant="subheadingMedium" style={{ marginBottom: "24px"}}>Information</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>Event and Class</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>About Us</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>Collab with Us</Text>
+          </Box>
+          <Box maxWidth="370px">
+            <Text variant="subheadingMedium" style={{ marginBottom: "24px"}}>Connect</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>connect@thesocialclub.id</Text>
+          </Box>
+          <Box maxWidth="370px">
+            <Text variant="subheadingMedium" style={{ marginBottom: "24px"}}>Contact</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>PT Karya Aplikasi Sosial Teknologi SCBD, Treasury Tower, 2nd Floor, Unit 2E District 8, Jl. Jenderal Sudirman No.52-53, RT.5/RW.3, Senayan, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12190</Text>
+          </Box>
+          <Box maxWidth="370px">
+            <Text variant="subheadingMedium" style={{ marginBottom: "24px"}}>Social Media</Text>
+            <img src={"/images/Instagram.svg"} alt="instagram" style={{marginRight: "8px"}}/>
+            <img src={"/images/linkedin.svg"} alt="linkedin"/>
+          </Box>
+          <Box maxWidth="370px">
+            <Text variant="subheadingMedium" style={{ marginBottom: "24px"}}>Others</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>Terms and Conditions</Text>
+            <Text variant="bodyLarge" style={{ marginBottom: "24px"}}>F.A.Q</Text>
+          </Box>
             {/* {
               shortcuts.map((el, idx) => (
                 <Box maxWidth="370px" key={idx}>
@@ -123,14 +124,14 @@ const Footer = () => {
         </InformationBox>
       </FadeUp>
       </FooterContentBox>
-      <FadeUp>
+      {/* <FadeUp>
         <Box display="flex" flexDirection="row" marginTop={1} >
           <img src="/images/Vector.svg" alt="logo"  style={{marginRight: theme.spacing(1)}} />
           <Text variant="anagataLightSmall" sx={{ color: theme.palette.white[50]}}> 
             2022 Anagata Visi Teknologi, All Rights Reserved
           </Text>
         </Box>
-      </FadeUp>
+      </FadeUp> */}
      
 
     </FooterContainer>
--- src/app/components/Footer/styles.js
+++ src/app/components/Footer/styles.js
@@ -8,7 +8,7 @@ export const FooterContainer = styled(Box)(({ theme }) => ({
   justifyContent: "center",
   alignItems: "center",
   padding: theme.spacing(4),
-  backgroundColor: theme.palette.black[50],
+  backgroundColor: "#2F3948",
   zIndex: 1,
   position: "relative",
   bottom: 0,
@@ -23,6 +23,7 @@ export const FooterContentBox = styled(Box)(({ theme }) => ({
   borderBottom: `2px solid ${theme.palette.white[50]}`,
   flexDirection: "column",
   position: "relative",
+  overflow: "wrap",
 }))
 
 export const InformationBox = styled(Box)(({ theme }) => ({
@@ -39,9 +40,11 @@ export const InformationBox = styled(Box)(({ theme }) => ({
 
 export const ShortcutContainer = styled(Box)(({ theme }) => ({
   display: "grid",
-  gridTemplateColumns: "repeat(3,1fr)",
-  gap: theme.spacing(1),
+  gridTemplateColumns: "repeat(4,1fr)",
+  gap: "50px",
+  width: "100%",
   marginBottom: theme.spacing(3),
+  color: "#FFFFFF",
   [theme.breakpoints.down("sm")]: {
     gridTemplateColumns: "repeat(2,1fr)",
     marginTop: theme.spacing(2)
